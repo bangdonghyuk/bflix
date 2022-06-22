ce314be1516f507c50f761c2b7b90b165878f70a
bangdonghyuk <yolo0528@naver.com>
2022-06-22 16:39:04 +0900

    Updates

---
+++ precache-manifest.b89ef050da69bd6a9055d64fc3286614.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7478c582aab9de1bd95e",
+    "url": "/bflix/static/css/main.32a724fc.chunk.css"
+  },
+  {
+    "revision": "7478c582aab9de1bd95e",
+    "url": "/bflix/static/js/main.7478c582.chunk.js"
+  },
+  {
+    "revision": "c71f08d1fbbd70a0a045",
+    "url": "/bflix/static/js/1.c71f08d1.chunk.js"
+  },
+  {
+    "revision": "0b461511727053c6a41a",
+    "url": "/bflix/static/js/runtime~main.0b461511.js"
+  },
+  {
+    "revision": "73f22e889f998e42e548d4b255d6a1cf",
+    "url": "/bflix/index.html"
+  }
+];
